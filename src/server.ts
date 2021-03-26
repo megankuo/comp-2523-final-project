3d0da1dfbc5d86a45a177a29007122c61f5775a3
iflinda <informlinda@gmail.com>
2021-03-25 22:10:25 -0700

    Testing cpx command

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import App from "./app";
+import App from "./App";
 import PostController from "./areas/post/controllers/post.controller";
 import AuthenticationController from "./areas/authentication/controllers/Authentication.controller";
 import { MockAuthenticationService } from "./areas/authentication/services/Authentication.service.mock";
