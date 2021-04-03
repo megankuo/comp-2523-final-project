7866905b67e82f8453a215449e41342ddc1d49e5
Megan Kuo <megan.kuo@live.com>
2021-04-03 00:15:06 -0700

    config using constructor

--- src/areas/authentication/config/PassportConfig.ts
+++ src/areas/authentication/config/PassportConfig.ts
@@ -3,4 +3,34 @@
 //----------------------------------------
 // 🚀 Configure Passport.js Local Authentication in this file
 //    Ensure code is fully typed wherever possible (unless inference can be made)
-export default class PassportConfig {}
+import * as passportLocal from "passport-local";
+import { IAuthenticationService } from "../services";
+
+const LocalStrategy = passportLocal.Strategy;
+
+export default class PassportConfig {
+  private _localLogin;
+  private _user;
+
+  constructor(authService: IAuthenticationService) {
+    this._localLogin = new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+      },
+      // check if user exists in the database
+      async (email, password, done) => {
+        this._user = await authService.getUserByEmailAndPassword(email, password);
+        return this._user
+          ? done(null, this._user)
+          : done(null, false, {
+              message: "Your login details are not valid. Please try again",
+            });
+      }
+    );
+  }
+
+  public get strategy(): passportLocal.Strategy {
+    return this._localLogin;
+  }
+}
